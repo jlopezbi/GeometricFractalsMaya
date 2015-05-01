88b46cf68794eb8a93ca2927a47ad40ec2aa2cc8
Josh Lopez-Binder <joshlobinder@gmail.com>
2015-05-01 14:14:31 -0700

    made makeChild function

--- makeLinearArray_script.py
+++ makeLinearArray_script.py
@@ -1,34 +1,40 @@
 import maya.cmds as cmds
 
-axes = ['X', 'Y', 'Z']
-translate = ['translate'+x for x in axes]
-rot = ['rotate'+x for x in axes]
-scale = ['scale'+x for x in axes]
-attrs = translate + rot + scale
-
-
-nCubes = 10
-
-
-baseObj = cmds.polyCube(w=1,d=1,h=1)[0]
-prevObj = baseObj
-newObjs = None
-
-for i in xrange(nCubes-1):
-
-    # DUPLICATE
-    num = i+2
-    newCubeName = 'pCube%s' %prevObj[-1]
-    newObj = cmds.duplicate(prevObj,n = newCubeName,rr=True,ilf=True)[0]
+def makeArray():
+    
+    
+    axes = ['X', 'Y', 'Z']
+    translate = ['translate'+x for x in axes]
+    rot = ['rotate'+x for x in axes]
+    scale = ['scale'+x for x in axes]
+    attrs = translate + rot + scale
+    
+    nCubes = 10
+    
+    
+    baseObj = cmds.polyCube(w=1,d=1,h=1)[0]
+    prevObj = baseObj
+    newObjs = None
+    
+    for i in xrange(nCubes-1):
+             
+        prevObj = makeChild(prevObj)
  
-   
+
+
+def makeChild(parentObj):
+     
+    # DUPLICATE
+    newCubeName = 'pCube%s' %parentObj[-1]
+    newObj = cmds.duplicate(parentObj,n = newCubeName,rr=True,ilf=True)[0]
+    
+    
     # PARENT
-    cmds.parent(newObj,prevObj)
+    cmds.parent(newObj,parentObj)
     
     # CONNECT XFORM ATTRS
     for attr in attrs:
-        cmds.expression(s = newObj+'.' +attr+ ' = ' + prevObj+'.' +attr)
+        cmds.expression(s = newObj+'.' +attr+ ' = ' + parentObj+'.' +attr)
     
-    prevObj = newObj
-    
-   
+    return newObj
+         
